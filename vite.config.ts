f0ebd53db2a5cbb9cf3d5fba246fa051519a1bc3
tpetrovic <tpetrovic@singidunum.ac.rs>
2025-05-14 19:10:34 +0200

    implemented env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import ui from '@nuxt/ui/vite'
 
+const env = loadEnv(
+  "all",
+  process.cwd()
+);
+
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: parseInt(env.VITE_PORT) || 5173,
+  },
   plugins: [
     vue(),
     vueDevTools(),
