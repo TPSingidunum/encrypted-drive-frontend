62273eca8e23970257110d159cc2223d44d9e36f
tpetrovic <tpetrovic@singidunum.ac.rs>
2025-05-16 18:39:01 +0200

    Implemted Auth routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/auth/LoginView.vue';
+import LoginView from '../views/auth/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView, },
-    { path: '/login', name: 'login', component: LoginView, },
+    { path: '/', name: 'home', component: HomeView, meta: { requiredAuth: true } },
+    { path: '/login', name: 'login', component: LoginView },
   ],
+})
+
+router.beforeEach((to, _from, next) => {
+  if (to.meta.requiredAuth) {
+    const token = localStorage.getItem('access_token')
+    if (!token) {
+      return next({ name: 'login' })
+    } else {
+      return next()
+    }
+  }
+
+  return next()
 })
 
 export default router
